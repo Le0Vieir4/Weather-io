2dcadd88181837b17f8dfe018e74770e1371f0df
Leo <lvm9508@gmail.com>
2025-11-27 09:23:09 -0300

    applying gitignore global

--- apps/shared/dist/user.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserSchema = void 0;
-const zod_1 = require("zod");
-exports.UserSchema = zod_1.z.object({
-    name: zod_1.z.string().optional(),
-    email: zod_1.z.string().email(),
-    password: zod_1.z.string().min(6)
-});
